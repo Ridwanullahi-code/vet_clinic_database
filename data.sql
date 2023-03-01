9fe2e917ecc10b02717e549513133658bc9c1e46
ridwanullahi <ridwanullahiajayi01@gmail.com.com>
2023-03-01 17:42:29 +0100

    complete database creation

--- data.sql
+++ data.sql
@@ -1,5 +1,10 @@
-/* Populate database with sample data. */
+SET datestyle = dmy;
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES(1, 'Agumon','02/03/2020',0,true,10.23);
+INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES(2, 'Gabumon','15/11/2018',2,true,8.0);
+INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES(3, 'Pikachu','07/01/2021',1,false,15.04);
+INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES(4, 'Devimon','12/05/2017',5,true,11.0);
--- queries.sql
+++ queries.sql
@@ -1,3 +1,27 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * FROM animals
+WHERE name LIKE '%mon'
+ORDER BY id ASC;
+
+SELECT name FROM animals
+WHERE extract(year from date_of_birth) BETWEEN '2016' AND '2019';
+
+SELECT name FROM animals
+WHERE neutered = TRUE AND escape_attempts < 3
+ORDER BY id ASC;
+
+SELECT TO_CHAR(date_of_birth:: date,  'Mon dd, yyyy') AS birth FROM animals
+WHERE name = 'Agumon' or name = 'Pikachu';
+
+SELECT name, escape_attempts from animals
+WHERE weight_kg > 10.5 ;
+
+SELECT * FROM animals
+WHERE neutered = true;
+
+SELECT * FROM animals
+WHERE name != 'Gabumon';
+
+SELECT * FROM animals
+WHERE weight_kg >= 10.4 AND weight_kg <= 17.3
